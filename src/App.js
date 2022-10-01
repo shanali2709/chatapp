a17a9bf5f0329f74b5dc37792ab3d264aced501b
Hans Lia <hanslia@Hanss-MacBook-Pro.local>
2022-10-01 11:23:28 -0400

    added text instead of emojis

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>chat to the world!</h1>
         <SignOut />
       </header>
 
@@ -104,7 +104,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>SEND</button>
 
     </form>
   </>)
